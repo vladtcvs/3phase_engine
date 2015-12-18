bf8bd45fac7b316bdb46ff47569adda0c6e73131
Vladislav Tsendrovskii <vlad@vlad.vltc.net.eu.org>
2015-12-18 18:09:07 +0300

    use new pins for out
    better shell

--- code-c.c
+++ code-c.c
@@ -10,13 +10,13 @@ typedef unsigned short word;
 #define SETBIT(x, n) ((x) |= (1 << n))
 #define CLRBIT(x, n) ((x) &= (0xFF - (1 << n)))
 
-#define VU 5
-#define VV 6
-#define VW 7
+#define EU 0
+#define EV 2
+#define EW 4
 
-#define XU 2
-#define XV 3
-#define XW 4
+#define UU 1
+#define UV 3
+#define UW 5
 
 #define PH_NUM 36
 byte sinus[PH_NUM];
@@ -24,8 +24,12 @@ byte sinus[PH_NUM];
 byte ampl, period;
 byte ph_u, ph_v, ph_w;
 register byte pwm_u asm("r10"), pwm_v asm("r11"), pwm_w asm("r12");
-byte rs232_len;
+byte rs232_len = 0;
 byte rs232_buf[4];
+
+#define TXS 8
+byte rs232_buf_tx[TXS];
+byte rs232_len_tx = 0;
 byte go;
 
 #define STOP 'B'
@@ -39,9 +43,9 @@ byte go;
 void
 setup_ports(void)
 {
-	DDRC  = 0x03;
+	DDRC  = 0x3F;
 	PORTC = 0x00;
-	DDRD  = 0xFF;
+	DDRD  = 0xF0;
 	PORTD = 0x00;
 }
 
@@ -81,53 +85,131 @@ ISR(TIMER0_COMPA_vect)
 	pwm_w = (sinus[ph_w]*((word)ampl))>>8;
 }
 
-ISR(USART_RX_vect)
+static void set_cw(void)
 {
-	byte t;
-	byte rcv = UDR0;
-	UDR0 = rcv;
-	rs232_buf[rs232_len++] = rcv;
-	switch(rs232_buf[0]) {
+	ph_v = (ph_u + 12)%PH_NUM;
+	ph_w = (ph_u + 24)%PH_NUM;
+}
+
+static void set_ccw(void)
+{
+	ph_w = (ph_u + 12)%PH_NUM;
+	ph_v = (ph_u + 24)%PH_NUM;
+}
+
+static void inverse(void)
+{
+	byte t = ph_v;
+	ph_v = ph_w;
+	ph_w = t;
+}
+
+static void start_stop(byte en)
+{
+	go = en;
+	if (en)
+		PORTD |= 0x10;
+	else
+		PORTD &= ~0x10;
+}
+
+static uint16_t read_uint(byte *arr, byte len)
+{
+	byte i = 0;
+	uint16_t res = 0;
+	while (i < len) {
+		res *= 10;
+		res += arr[i] - '0';
+		i++;
+	}
+	return res;
+}
+
+static void set_amplitude(void)
+{
+	uint16_t rw = read_uint(&rs232_buf[1], rs232_len - 1);
+	ampl = rw < 256 ? rw : 255;
+}
+
+static void set_freq(void)
+{
+	uint16_t rw = read_uint(&rs232_buf[1], rs232_len - 1);
+	period = rw < 256 ? rw : 255;
+}
+
+void shell_handle(void)
+{
+	switch (rs232_buf[0]) {
 	case 'B':
-		go = 0;
-		rs232_len = 0;
+		start_stop(0);
 		break;
 	case 'G':
-		go = 1;
-		rs232_len = 0;
+		start_stop(1);
 		break;
 	case 'C':
-		ph_v = (ph_u + 12)%PH_NUM;
-		ph_w = (ph_u + 24)%PH_NUM;
-		rs232_len = 0;
-		break;
+		set_cw();
+ 		break;
 	case 'W':
-		ph_v = (ph_u + 24)%PH_NUM;
-		ph_w = (ph_u + 12)%PH_NUM;
-		rs232_len = 0;
+		set_ccw();
 		break;
-	case 'R':
-		t = ph_v;
-		ph_v = ph_w;
-		ph_w = t;
-		rs232_len = 0;
-		break;
-	case 'P':
-		if (rs232_len == 2) {
-			period = rs232_buf[1];
-			rs232_len = 0;
-		}
+	case 'I':
+		inverse();
 		break;
 	case 'A':
-		if (rs232_len == 2) {
-			ampl = rs232_buf[1];
-			rs232_len = 0;
-		}
+		set_amplitude();
 		break;
-	default:
-		rs232_len = 0;
+	case 'F':
+		set_freq();
 		break;
 	}
+}
+
+ISR(USART_TX_vect)
+{
+	byte i;
+	for (i = 0; i < rs232_len_tx - 1; i++)
+		rs232_buf_tx[i] = rs232_buf_tx[i+1];
+	if (rs232_len_tx > 0)
+		rs232_len_tx--;
+	if (rs232_len_tx > 0)
+		UDR0 = rs232_buf_tx[0];
+	else
+		UCSR0B &= ~(1<<TXCIE0);
+}
+
+static void tx_byte(byte c)
+{
+	UCSR0B &= ~(1<<TXCIE0);
+	if (rs232_len_tx == 0) {
+		rs232_buf_tx[0] = c;
+		UDR0 = c;
+		rs232_len_tx = 1;
+	} else if (rs232_len_tx < TXS) {
+		rs232_buf_tx[rs232_len_tx++] = c;
+		UCSR0B |= (1<<TXCIE0);
+	}
+}
+
+ISR(USART_RX_vect)
+{
+	byte rcv = UDR0;
+	UCSR0B &= ~(1<<RXCIE0);
+	switch (rcv) {
+	case '\r':
+		shell_handle();
+		rs232_len = 0;
+		tx_byte('\r');
+		tx_byte('\n');
+		tx_byte('>');
+		tx_byte(' ');
+		break;
+	case '\n':
+		break;
+	default:
+		rs232_buf[rs232_len++] = rcv;
+		tx_byte(rcv);
+	}
+	UCSR0B |= (1<<RXCIE0);
 }
 
 void
@@ -164,42 +246,47 @@ int main(void)
 	ph_u = 0;
 	ph_v = 24;
 	ph_w = 12;
-	pwm_u = 10;
-	pwm_v = 50;
-	pwm_w = 50;
+	pwm_u = 0;
+	pwm_v = 0;
+	pwm_w = 0;
 
 	sei();
 	while (1) {
 		register byte i;
 		if (go) {
-			SETBIT(PORTD, XU); 
-			SETBIT(PORTD, XV); 
-			SETBIT(PORTD, XW); 
+			CLRBIT(PORTC, EU); 
+			CLRBIT(PORTC, EV); 
+			CLRBIT(PORTC, EW); 
 
-			CLRBIT(PORTD, VU);
-			CLRBIT(PORTD, VV);
-			CLRBIT(PORTD, VW);
+			SETBIT(PORTC, UU);
+			SETBIT(PORTC, UV);
+			SETBIT(PORTC, UW);
+
+			_delay_loop_1(50);
+
+			SETBIT(PORTC, EU); 
+			SETBIT(PORTC, EV); 
+			SETBIT(PORTC, EW); 
 
 			for (i = 0; i < 100; i++) {
 				if (i == pwm_u) {
-					SETBIT(PORTD, VU); // DISABLE U
-					CLRBIT(PORTD, XU);  // Set LOW
+					CLRBIT(PORTC, EU); // DISABLE U
+					CLRBIT(PORTC, UU);  // Set LOW
 				}
 				if (i == pwm_v) {
-					SETBIT(PORTD, VV); // DISABLE VU
-					CLRBIT(PORTD, XV);  // Set LOW
+					CLRBIT(PORTC, EV); // DISABLE VU
+					CLRBIT(PORTC, UV);  // Set LOW
 				}
 				if (i == pwm_w) {
-					SETBIT(PORTD, VW); // DISABLE W
-					CLRBIT(PORTD, XW);  // Set LOW
+					SETBIT(PORTC, EW); // DISABLE W
+					CLRBIT(PORTC, UW);  // Set LOW
 				}
 				_delay_loop_1(50);
-				CLRBIT(PORTD, VU);
-				CLRBIT(PORTD, VV);
-				CLRBIT(PORTD, VW);
+				SETBIT(PORTC, EU);
+				SETBIT(PORTC, EV);
+				SETBIT(PORTC, EW);
 			}
 		}
-		PORTD = (1 << VU) | (1 << VV) | (1 << VW);
 		_delay_loop_1(50);
 	}
 	return 0;
