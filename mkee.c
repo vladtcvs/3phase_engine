1cf22761b48650a85884873b50c36ca5a722403b
Vladislav Tsendrovskii <vlad@vlad.vltc.net.eu.org>
2015-12-18 15:08:53 +0300

    more checks in mkee.c

--- mkee.c
+++ mkee.c
@@ -2,10 +2,24 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *f = fopen(argv[2], "rt");
-	FILE *fo = fopen(argv[1], "wt");
+	FILE *f;
+	FILE *fo; 
 	int i;
 	int c;
+	if (argc != 5) {
+		printf("Usage: mkee eesin.bin sin.txt ampl.txt freq.txt");
+		return 1;
+	}
+	f = fopen(argv[2], "rt");
+	if (f == NULL) {
+		printf("File %s not found\n", argv[2]);
+		return 1;
+	}
+	fo = fopen(argv[1], "wt");
+	if (fo == NULL) {
+		printf("Can not open file %s for write\n", argv[1]);
+		return 1;
+	}
 	for (i = 0; i < 36; i++) {
 		fscanf(f, "%i", &c);
 		fprintf(fo, "%c", (char)c);
@@ -13,11 +27,19 @@ int main(int argc, char *argv[])
 	fclose(f);
 	
 	f = fopen(argv[3], "rt");
+	if (f == NULL) {
+		printf("File %s not found\n", argv[3]);
+		return 1;
+	}
 	fscanf(f, "%i", &c);
 	fprintf(fo, "%c", (char)c);
 	fclose(f);
 
 	f = fopen(argv[4], "rt");
+	if (f == NULL) {
+		printf("File %s not found\n", argv[4]);
+		return 1;
+	}
 	fscanf(f, "%i", &c);
 	fprintf(fo, "%c", (char)c);
 	fclose(f);
